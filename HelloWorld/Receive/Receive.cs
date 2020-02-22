617995d84e18b9d18448708e4adfd3ab811ef851
Evgeniy Pilyaev <evgeniypilyaev@gmail.com>
2020-02-22 12:48:38 +0200

    Add Receive, resulting output

--- HelloWorld/Receive/Receive.cs
+++ HelloWorld/Receive/Receive.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Receive
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     class Receive
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare("hello", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            
+            channel.BasicConsume("hello", true, consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
