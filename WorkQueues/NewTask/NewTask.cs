2cfcd74809cc1371a15e5ee6745073c8cde2c38a
Evgeniy Pilyaev <evgeniypilyaev@gmail.com>
2020-02-24 23:02:40 +0200

    Add WorkQueues-NewTask (start)

---
+++ WorkQueues/NewTask/NewTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    class NewTask
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare("task_queue", true, false, 
+                false, null);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish("",
+                "task_queue",
+                properties,
+                body);
+            Console.WriteLine(" [x] Sent {0}", message);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Length > 0 ? string.Join(" ", args) : "Hello World!";
+        }
+    }
+}
