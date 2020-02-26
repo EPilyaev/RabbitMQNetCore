4aee6a3bd97472a1b294f4e133e23930ddce14c8
Evgeniy Pilyaev <evgeniypilyaev@gmail.com>
2020-02-26 22:24:10 +0200

    Start Worker, add Dispatching

--- WorkQueues/Worker/Worker.cs
+++ WorkQueues/Worker/Worker.cs
@@ -6,45 +6,44 @@ using RabbitMQ.Client.Events;
 
 namespace Worker
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     class Worker
     {
         public static void Main()
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            using(var connection = factory.CreateConnection())
-            using(var channel = connection.CreateModel())
+            
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            channel.QueueDeclare("task_queue", true, false,
+                false, null);
+
+            channel.BasicQos(0, 1, false);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
             {
-                channel.QueueDeclare(queue: "task_queue",
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
 
-                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
 
-                Console.WriteLine(" [*] Waiting for messages.");
+                Console.WriteLine(" [x] Done");
 
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+            
+            channel.BasicConsume("task_queue",
+                false,
+                consumer);
 
-                    int dots = message.Split('.').Length - 1;
-                    Thread.Sleep(dots * 1000);
-
-                    Console.WriteLine(" [x] Done");
-
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-                };
-                channel.BasicConsume(queue: "task_queue",
-                    autoAck: false,
-                    consumer: consumer);
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
