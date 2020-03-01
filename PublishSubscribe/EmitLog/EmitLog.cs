ba2d1c3361ede7179e2d0fc659ab9725807258bf
Evgeniy Pilyaev <evgeniypilyaev@gmail.com>
2020-03-01 20:13:46 +0200

    Add Emit log slides

---
+++ PublishSubscribe/EmitLog/EmitLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace EmitLog
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    class EmitLog
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("logs", "", null, body);
+            Console.WriteLine(" [x] Sent {0}", message);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args) =>
+            args.Length > 0 ? string.Join(" ", args) : "info: Hello World!";
+    }
+}
---
+++ PublishSubscribe/ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogs
+{
+    class ReceiveLogs
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queueName,
+                "logs",
+                "");
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+            };
+            channel.BasicConsume(queueName,
+                true,
+                consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
