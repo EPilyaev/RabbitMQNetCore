65491aae3ac040031975ab745f93ad4cf3de2c36
Evgeniy Pilyaev <evgeniypilyaev@gmail.com>
2020-03-06 21:02:58 +0200

    Add slides and projects for direct logging system

---
+++ DirectLogging/ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogsDirect
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    class ReceiveLogsDirect
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("direct_logs",
+                    "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if(args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                        Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach(var severity in args)
+                {
+                    channel.QueueBind(queueName,
+                        "direct_logs",
+                        severity);
+                }
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                        routingKey, message);
+                };
+                channel.BasicConsume(queueName,
+                    true,
+                    consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
